d5201c9d660fa6c6da8440a57b34c83478ee4392
Ktracey1 <kayembekat1@nku.edu>
2025-04-27 23:15:19 -0400

    Apply-script.js

---
+++ Apply-script.js
@@ -0,0 +1,8 @@
+<script>
+  const menuButton = document.getElementById('menu-button');
+  const menu = document.getElementById('menu');
+
+  menuButton.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+</script>
